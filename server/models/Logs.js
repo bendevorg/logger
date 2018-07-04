94c20fdf89773b6a3a92ea41afeea2af5ed48154
Guilherme Conti <guibasconti@gmail.com>
2018-07-04 02:31:20 -0300

    Define log model

--- server/models/Logs.js
+++ server/models/Logs.js
@@ -1,9 +1,26 @@
-const mongoose  = require('mongoose');
-const Schema    = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const LogSchema = new Schema({
-  content: {type: Object, required: true},
+  req: {
+    base: { type: String, required: true },
+    url: { type: String, required: true },
+    path: { type: String, required: true },
+    method: { type: String, required: true },
+    date: { type: Date, required: true },
+    headers: { type: Schema.Types.Mixed, require: false },
+    params: { type: Schema.Types.Mixed, require: false },
+    query: { type: Schema.Types.Mixed, require: false },
+    body: { type: Schema.Types.Mixed, require: false }
+  },
+  res: {
+    statusCode: { type: Number, required: true },
+    date: { type: Date, required: true },
+    time: { type: Number, required: true },
+    headers: { type: Schema.Types.Mixed, require: false },
+    data: { type: Schema.Types.Mixed, require: false }
+  },
   timestamp: { type: Date, default: Date.now }
 });
 
-mongoose.model('Log', LogSchema);
+mongoose.model("Log", LogSchema);
