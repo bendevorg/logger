e3bc5aa230b506c00502d9b85bc1ffb75365960c
Guilherme Conti <guibasconti@gmail.com>
2019-06-11 18:56:52 -0300

    Enable CORS

--- tools/serverProduction.js
+++ tools/serverProduction.js
@@ -6,10 +6,22 @@ dotenv.config();
 require('../server/models/database');
 const express = require('express');
 const router = require('../server/core/router.js');
+const cors = require('cors');
 const logger = require('./logger');
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [`http://${process.env.FRONTEND_HOST}`,
+      `https://${process.env.FRONTEND_HOST}`,
+      `http://${process.env.BACKEND_HOST}`,
+      `https://${process.env.BACKEND_HOST}`
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    credentials: true
+  })
+);
 app.use(router);
 app.use(logger.errorHandler());
 
