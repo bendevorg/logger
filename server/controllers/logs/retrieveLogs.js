7c43d894f3bec9b87dd2f697e9706812048910e4
Guilherme Conti <guibasconti@gmail.com>
2019-06-11 18:16:22 -0300

    Add amount and offset parameters to retrieve logs api

--- server/controllers/logs/retrieveLogs.js
+++ server/controllers/logs/retrieveLogs.js
@@ -2,14 +2,21 @@ const mongoose = require('mongoose');
 const LogsModel = mongoose.model('Logs');
 
 module.exports = (req, res) => {
-  LogsModel.find(function (err, LogsModel) {
-    if (err) {
-      return res.status(500).json({
-        msg: err
+  const { amount, offset } = req.query;
+
+  LogsModel
+    .find({})
+    .sort({ 'req.date': -1 })
+    .limit(isNaN(amount) ? 0 : Number(amount))
+    .skip(isNaN(offset) ? 0 : Number(offset))
+    .exec((err, logs) => {
+      if (err) {
+        return res.status(500).json({
+          msg: err
+        });
+      }
+      return res.status(200).json({
+        msg: logs
       });
-    }
-    return res.status(200).json({
-      msg: LogsModel
     });
-  });
 };
